394b8b632f6e7199de89aad83cdd6e047edaf2a8
Guilherme Da Costa Barreto <guibarr3to@gmail.com>
2022-10-18 22:43:24 -0300

    Aula 6

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <HelloWorld/>
       <br/>
       <hr/>
-      <Pessoa nome="Guilherme" profissao="Progamador" idade="11" foto="https://steamuserimages-a.akamaihd.net/ugc/1455176870695154047/6AE7ADF9C609EB2256F5308EF8D8D0ABB82F986B/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false"/>
+      <Pessoa nome="Guilherme" profissao="Progamador" idade="11" foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjm0GKV-I6M_x81LG_0l_oPh_eFlsDCOonzQ&usqp=CAU"/>
       <Pessoa nome="yashiro" profissao="empregada de fantasma" idade="15" foto="https://s4.uupload.ir/files/88d8e5ffe666d74a2b286db0b9d7fc7c_521k.jpg"/>
     </div>
   );

475195730fe4d5961e5a3cb5986b0609c16a4648
Guilherme Da Costa Barreto <guibarr3to@gmail.com>
2022-10-19 14:35:54 -0300

    Aula 7

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import HelloWorld from './components/HelloWorld';
-import Frase from './components/Frase';
-import SayMyName from './components/SayMyName';
+import List from './components/List';
 import Pessoa from './components/Pessoa';
 
 function App() {
@@ -12,6 +11,7 @@ function App() {
       <hr/>
       <Pessoa nome="Guilherme" profissao="Progamador" idade="11" foto="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjm0GKV-I6M_x81LG_0l_oPh_eFlsDCOonzQ&usqp=CAU"/>
       <Pessoa nome="yashiro" profissao="empregada de fantasma" idade="15" foto="https://s4.uupload.ir/files/88d8e5ffe666d74a2b286db0b9d7fc7c_521k.jpg"/>
+      <List/>
     </div>
   );
 }
---
+++ my-app/src/components/List.js
@@ -0,0 +1,13 @@
+function list() {
+    return (
+        <>
+            <h2>Minha lista</h2>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+            </ul>
+        </>
+    )
+}   
+
+export default list
